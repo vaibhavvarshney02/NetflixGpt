2ad79661e264c0c15861bcceb09d0c16cc29d6b2
vaibhavvarshney02 <vaibhavvarshney867@gmail.com>
2024-07-19 19:17:49 +0530

    initial commit

---
+++ netflix-gpt/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+   <h1 className='bg-green-400'>kkkkk</h1>
+    </div>
+  );
+}
+
+export default App;
