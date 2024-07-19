7cf77abb7559fda3f601dcebfb136c3a8e2d435e
vaibhavvarshney02 <vaibhavvarshney867@gmail.com>
2024-07-19 19:25:30 +0530

    initial commit

--- netflix-gpt/src/App.js
+++ netflix-gpt/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-   <h1 className='bg-green-400'>kkkkk</h1>
+   <h1 className='bg-green-400'>kkfffffffffffkkk</h1>
     </div>
   );
 }
